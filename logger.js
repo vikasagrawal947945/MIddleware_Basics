bd49ae06d28b3a12dd15ec421ac43d23788972e1
vikas <>
2025-02-22 16:16:05 +0530

    req object

--- logger.js
+++ logger.js
@@ -1,15 +1,15 @@
- const express= require("express");
-  const app = express();
- app.use((req,res,next)=>{
-     console.log(req.method , req.hostname , req.path , req.time);
-     next();
- });
-
- app.get("/" , (req,res)=>{
-    res.send("hii");
- });
-
-
+const express = require("express");
+const app = express();
+ 
+app.use((req,res,next)=>{
+    let {token} = req.query; 
+     if(token === "Access") next();
+      else res.send("Access denied");
+});
+app.get("/" ,(req,res)=>{
+   res.send("hiii");
+});
+  
    app.listen(3000, ()=>{
     console.log("server running on port 3000");
    })
